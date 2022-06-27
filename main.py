ea255c4e8ca5240c8ea422ea487bfb44f5d08985
Александр Бугров <bugor9682@gmail.com>
2022-06-27 10:33:41 +0300

    task* in progress

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import itertools
+
 nested_list = [
     ['a', 'b', 'c'],
-    ['d', 'e', 'f', 'h', False],
-    [1, 2, None],
+    [['d', 'e'], ['f', 'h', False]],
+    [1, 2, None,],
 ]
 
 
@@ -36,12 +38,72 @@ def flat_generator(your_list, elem_num=0, item_num=0 - 1):
             item_num = 0 - 1
 
 
+
+class FlatIteratorTest:
+
+    def __init__(self, your_list: list):
+        self.your_list = your_list
+        self.list_range = len(self.your_list)
+        self.my_list_iterator = iter(self.your_list)
+
+    def __iter__(self):
+        self.myitem = []
+        self.myitem2 = 0
+        return self
+
+    def __next__(self):
+        # if type(self.your_list[self.elem_num]) == list:
+        #     list_range = len(self.your_list[self.elem_num])
+        #     my_list_iterator = iter(self.your_list[self.elem_num])
+        #     item = next(my_list_iterator)
+        #     if type(item) == list:
+        # if type(self.myitem) == list:
+
+        self.myitem = next(self.my_list_iterator)
+        if type(self.myitem) == list:
+            index = self.your_list.index(self.myitem)
+            self.your_list.insert(index)
+            del(self.your_list[index+1])
+
+def list_redactor(your_list):
+    list_in_list = []
+    while type(list_in_list) == list:
+        a = len(your_list)
+        for num, obg in enumerate(your_list):
+            if type(obg) is list:
+                for obg2 in obg:
+                    your_list.append(obg2)
+            else:
+                your_list.append(obg)
+            if num == a - 1:
+                break
+        i = 0
+        while i < a:
+            del(your_list[0])
+            i += 1
+        for obg in your_list:
+            if type(obg) == list:
+                list_in_list = []
+                break
+            else:
+                list_in_list = 0
+    return your_list
+
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
-    for item in FlatIterator(nested_list):
-        print(item)
-
-    flat_list = [item for item in FlatIterator(nested_list)]
-    print(flat_list)
-
-    for item in flat_generator(nested_list):
-        print(item)
+    # for item in FlatIterator(nested_list):
+    #     print(item)
+    #
+    # flat_list = [item for item in FlatIterator(nested_list)]
+    # print(flat_list)
+    #
+    # for item in flat_generator(nested_list):
+    #     print(item)
+    print(list_redactor(nested_list))
