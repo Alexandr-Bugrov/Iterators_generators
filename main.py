cf12728c204a46d855cc2ee2cf577ecf81277f82
Александр Бугров <bugor9682@gmail.com>
2022-07-02 11:40:25 +0300

    iterators stak and recursion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 nested_list = [
-    ['a', 'b', 'c'],
+    [[['a', 'b', 'c']]],
     ['d', 'e', 'f', 'h', False],
     [1, 2, None]
 ]
@@ -101,23 +101,82 @@ def flat_generatortask4(your_list):
             else:
                 list_in_list = 0
         
+class FlatIteratorV2:
+
+    def __init__(self, multi_list):
+
+        self.multi_list = multi_list  # список с воложенными списками
+
+    def __iter__(self):
+        self.multi_list_iter = iter(self.multi_list)
+        self.nested_list = []  # вложенный список с элементами
+        self.nested_list_cursor = -1
+        return self
+
+    def __next__(self):
+        self.nested_list_cursor += 1
+        if len(self.nested_list) == self.nested_list_cursor:
+            self.nested_list = None
+            self.nested_list_cursor = 0
+            while not self.nested_list:
+                self.nested_list = next(self.multi_list_iter)
+                #  если  список пустой, то получаем следующий
+                #  если списки закончаться, получим stop iteration
+
+        return self.nested_list[self.nested_list_cursor]
+
+class FlatIteratorV3:
+
+    def __init__(self, multi_list):
+        self.multi_list = multi_list
+
+    def __iter__(self):
+        self.iterators_stack = [iter(self.multi_list)]  # стэк иетраторов
+        return self
+    def __next__(self):
+        while self.iterators_stack:
+            try:
+                element = next(self.iterators_stack[-1])
+            except StopIteration:
+                self.iterators_stack.pop()
+                continue
+            if isinstance(element, list):
+                element_iter = iter(element)
+                self.iterators_stack.append(element_iter)
+            else:
+                return element
+        raise StopIteration
+
+
+def generatorV2(my_list):
+    for item in my_list:
+         if isinstance(item, list):
+             for subitem in generatorV2(item):
+                 yield subitem
+         else:
+             yield item
 
 if __name__ == '__main__':
-    for item in FlatIterator(nested_list):
-        print(item)
-    print('-' * 50)
+    # for item in FlatIterator(nested_list):
+    #     print(item)
+    # print('-' * 50)
+    #
+    # flat_list = [item for item in FlatIterator(nested_list)]
+    # print(flat_list)
+    # print('-' * 50)
+    #
+    # for item in flat_generator(nested_list):
+    #     print(item)
+    # print('-' * 50)
+    #
+    # for item in FlatIteratorTask3(nested_list):
+    #     print(item)
+    # print('-' * 50)
+    #
+    # for item in flat_generatortask4(nested_list):
+    #     print(item)
 
-    flat_list = [item for item in FlatIterator(nested_list)]
-    print(flat_list)
-    print('-' * 50)
 
-    for item in flat_generator(nested_list):
+    for item in generatorV2(nested_list):
         print(item)
-    print('-' * 50)
 
-    for item in FlatIteratorTask3(nested_list):
-        print(item)
-    print('-' * 50)
-
-    for item in flat_generatortask4(nested_list):
-        print(item)
